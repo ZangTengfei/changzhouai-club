e8a5b58b9a8c9c2fc20a7b078863e5ff247998a9
Tengfei Zang <zangtf@gmail.com>
2026-04-24 03:16:44 +0800

    Bust site logo cache

--- src/components/site-logo-mark.tsx
+++ src/components/site-logo-mark.tsx
@@ -1,19 +1,16 @@
-import Image from "next/image";
-
 type SiteLogoMarkProps = {
   className?: string;
 };
 
+const SITE_LOGO_SRC = "/logo.png?v=20260424";
+
 export function SiteLogoMark({ className }: SiteLogoMarkProps) {
   return (
-    <Image
-      src="/logo.png"
+    <img
+      src={SITE_LOGO_SRC}
       alt=""
       aria-hidden="true"
       className={className}
-      width={512}
-      height={512}
-      priority
     />
   );
 }
