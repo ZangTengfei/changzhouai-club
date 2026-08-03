39ff118ef389f2fdd59be8f8f182793e42e643a6
Tengfei Zang <zangtf@gmail.com>
2026-08-03 23:41:25 +0800

    feat(miniapp): 采用小程序风格主页分享封面

--- miniapp/miniprogram/pages/home/index.ts
+++ miniapp/miniprogram/pages/home/index.ts
@@ -18,7 +18,7 @@ type HomeEvent = EventSummary & {
 };
 
 const weekdayLabels = ["周日", "周一", "周二", "周三", "周四", "周五", "周六"];
-const homeShareImageUrl = "/assets/share/home-share-v6.jpg";
+const homeShareImageUrl = "/assets/share/home-share-v7.jpg";
 
 function formatBriefDate() {
   const now = new Date();
